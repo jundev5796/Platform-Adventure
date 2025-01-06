cc2371ccaa53289321660dd1be1bbff996e80acf
HyunJun Baek <hjbaek5796@gmail.com>
2025-01-07 04:59:07 +0900

    set script environment

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,12 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        rb.linearVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), rb.linearVelocity.y);
+    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
     }
 }
