7ea0d2568ab85ec00739be874aae095a68eef413
HyunJun Baek <hjbaek5796@gmail.com>
2025-01-09 02:29:07 +0900

    [Player] implement player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] private float speed;
     
-    private void Awake() {
+    [SerializeField] private float moveSpeed;
+
+    private float xInput;
+    
+    private void Awake() 
+    {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() 
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
     }
 }
