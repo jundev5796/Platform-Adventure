bb155ed1746531df29a7bfa0d69604d8fcc49623
HyunJun Baek <hjbaek5796@gmail.com>
2025-01-09 16:18:07 +0900

    [Player] transition between idle and move animations

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
 
     [SerializeField] private float moveSpeed;
 
@@ -12,17 +13,24 @@ public class Player : MonoBehaviour
     private void Awake() 
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    private void Update() 
+    private void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
-
+        
+        HandleAnimations();
         HandleMovement();
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetBool("isRunning", rb.linearVelocityX != 0);
     }
 
     private void HandleMovement() 
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
     }
 }
