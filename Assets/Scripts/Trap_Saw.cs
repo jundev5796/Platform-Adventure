c938f9695ad2278c6733d09c650f0db6d95953fc
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-27 19:36:17 +0900

    Update saw trap to move between waypoints

--- Assets/Scripts/Trap_Saw.cs
+++ Assets/Scripts/Trap_Saw.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class Trap_Saw : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed = 3;
+    [SerializeField] private Transform[] wayPoint;
+
+    public int wayPointIndex = 1;
+
+
+    private void Start()
+    {
+        transform.position = wayPoint[0].position;
+    }
+
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint[wayPointIndex].position, moveSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, wayPoint[wayPointIndex].position) < 0.1f)
+        {
+            wayPointIndex++;
+
+            if (wayPointIndex >= wayPoint.Length)
+                wayPointIndex = 0;
+        }
+    }
 }
