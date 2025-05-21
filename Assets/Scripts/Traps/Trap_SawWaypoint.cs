e70a6e5e117a2ace725f987f77eed9bc07683ebe
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-21 20:26:57 +0900

    push(2)

--- Assets/Scripts/Traps/Trap_SawWaypoint.cs
+++ Assets/Scripts/Traps/Trap_SawWaypoint.cs
@@ -2,15 +2,5 @@ using UnityEngine;
 
 public class Trap_SawWaypoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // We need it to detect object
 }
